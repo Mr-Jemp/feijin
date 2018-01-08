b3020f1734c2a7e0685a0dea1d16c7b45a5f46e1
Mr-Jemp <1303232158@qq.com>
2018-01-08 16:03:18 +0800

    修改底部样式，链接补充

--- 飞进官网/resources/js/indexHeader.js
+++ 飞进官网/resources/js/indexHeader.js
@@ -1,32 +1,32 @@
 (function() {
-    /************操作头部导航区的样式************/
-    var oHeader = $(".public-header");
-    var oLogo = $(".public-header img");
-    var scrollTop = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;
-    if(scrollTop > 20) { //页面刷新保留header的样式操作
-        oHeader.css({
-            "background-color": "#fff",
-            "color": "#000"
-        });
-        oLogo.attr("src", "./resources/img/logo_color.png");
-    }
+	/************操作头部导航区的样式************/
+	var oHeader = $(".public-header");
+	var oLogo = $(".public-header img");
+	var scrollTop = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;
+	if(scrollTop > 20) { //页面刷新保留header的样式操作
+		oHeader.css({
+			"background-color": "#fff",
+			"color": "#000"
+		});
+		oLogo.attr("src", "./resources/img/logo_color.png");
+	}
 
-    function setHeadStyle() {
-        //实时获取滚动条距离顶部的值
-        scrollTop = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;
-        if(scrollTop > 20) {
-            oHeader.css({
-                "background-color": "#fff",
-                "color": "#000"
-            });
-            oLogo.attr("src", "./resources/img/logo_color.png");
-        } else {
-            oHeader.css({
-                "background-color": "transparent",
-                "color": "#fff"
-            });
-            oLogo.attr("src", "./resources/img/logo.png");
-        }
-    }
-    $(window).on("scroll", setHeadStyle)
+	function setHeadStyle() {
+		//实时获取滚动条距离顶部的值
+		scrollTop = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;
+		if(scrollTop > 20) {
+			oHeader.css({
+				"background-color": "#fff",
+				"color": "#000"
+			});
+			oLogo.attr("src", "./resources/img/logo_color.png");
+		} else {
+			oHeader.css({
+				"background-color": "transparent",
+				"color": "#fff"
+			});
+			oLogo.attr("src", "./resources/img/logo.png");
+		}
+	}
+	$(window).on("scroll", setHeadStyle);
 })();
