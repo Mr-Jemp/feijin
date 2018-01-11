af5a41fac73451f483e55199f91a710ab2941412
Mr-Jemp <1303232158@qq.com>
2018-01-11 18:27:03 +0800

    面包屑导航补充

--- 飞进官网/resources/js/case.js
+++ 飞进官网/resources/js/case.js
@@ -1,8 +1,10 @@
 ;(function(){
   var $nav = $(".case-2 .case2-nav>li");
   var $list = $(".case-2 .case2-list");
+  var $bread = $(".case-2 .bread-nav .change");
   var index;
   $nav.click(function(){
+  	$bread.text($(this).text()).attr("title",$(this).text());
     index = $(this).index();
     $(this).addClass("active").siblings("li").removeClass("active");
     $list.stop(true,true).fadeOut(200).eq(index).stop(true,true).fadeIn(500);
--- 飞进官网/resources/js/info.js
+++ 飞进官网/resources/js/info.js
@@ -1,8 +1,10 @@
 (function(){
   var $nav = $(".info-2 .info2-nav>li");
   var $list = $(".info-2 .list-1");
+  var $bread = $(".info-2 .bread-nav .change");
   var index;
   $nav.click(function(){
+  	$bread.text($(this).text()).attr("title",$(this).text());
     index = $(this).index();
     $(this).addClass("active").siblings("li").removeClass("active");
     $list.stop(true,true).fadeOut(200).eq(index).stop(true,true).fadeIn(500);
